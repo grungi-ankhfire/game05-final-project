c2f9436c8dfc2e11fa7af71ec0105bf4e16a5aec
Bastien Gorissen <bastien.gorissen@interface3.be>
2020-09-25 10:28:14 +0200

    Imported Unity project to Git

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public GameObject target;
+
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Debug.Log("GAME OVER - COLLISION");
+        }    
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("GAME OVER!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = target.transform.position;
+    }
+
+}
